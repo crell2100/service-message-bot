0e04451553ef3b35addadd256c76fe85ba2ffd20
crell2100 <crell2100@gmail.com>
2024-12-14 16:53:48 +0300

    start aiogram

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import telebot
 
-img = open("pic.jpg", 'rb')
-
 import config
 
 bot=telebot.TeleBot(config.Token)
 
 @bot.message_handler()
 def service_message(message):
-    if config.Send_picture and config.Send_text:
-        bot.send_photo(message.chat.id, img, caption=config.Text)
-    elif config.Send_picture:
-        bot.send_photo(message.chat.id, img)
-    elif config.Send_text:
-        bot.send_message(message.chat.id,config.Text)
+    with open("pic.jpg",'rb') as pic:
+        if config.Send_picture and config.Send_text:
+            bot.send_photo(message.chat.id, pic, caption=config.Text)
+        elif config.Send_picture:
+            bot.send_photo(message.chat.id, pic)
+        elif config.Send_text:
+            bot.send_message(message.chat.id,config.Text)
 
-bot.polling(none_stop=True, interval=1)
+if __name__ == '__main__':
+    bot.polling()
